43ab7d3e0e64469a69020381161ab30694269900
DesWolf <okuneev@gmail.com>
2020-02-18 11:41:36 +0700

    Initial Commit

---
+++ LifecycleViewControllerExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifecycleViewControllerExample
+//
+//  Created by Максим Окунеев on 2/18/20.
+//  Copyright © 2020 Максим Окунеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
